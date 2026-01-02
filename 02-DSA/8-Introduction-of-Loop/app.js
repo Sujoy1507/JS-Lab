aec296450c1250f9e41f8584b60358abc90d225f
Sujoy Saha <ace.sujoy@gmail.com>
2026-01-02 10:18:31 +0530

    DSA Jan 2

---
+++ 02-DSA/8-Introduction-of-Loop/app.js
@@ -0,0 +1,14 @@
+// let prompt = require("prompt-sync")();
+
+// let n = Number(prompt("Enter Any Number"));
+
+// for (let i = 0; i < n; i++) {
+//     console.log("Hello Sujoy");
+// }
+
+let n =5;
+let fact = 1;
+for (let i = 1;i<=n;i++){
+    fact*=i;
+    console.log(fact)
+}
