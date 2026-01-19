fb28aca1661a84ead72fee52186f44da4db4400b
Sujoy Saha <ace.sujoy@gmail.com>
2026-01-19 12:40:58 +0530

    Star Patern

---
+++ 02-DSA/16-Pattern-programming/starPrintRowColumn.js
@@ -0,0 +1,17 @@
+let prompt = require("prompt-sync")();
+let n = Number(prompt("Behenchod Kuch To Number Likh n ke liye -:- "));
+
+for(let i = 1 ; i<=n;i++){
+    for(let j=1;j<=n-i+1;j++){
+        process.stdout.write(':: ')
+    }
+    console.log()
+}
+
+
+for (let i = 1; i<=n;i++){
+    for(let j = n;j>=i;j--){
+        process.stdout.write('* ')
+    }
+    console.log()
+}
